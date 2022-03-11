18b4462237dc64ca4aa124a022571d804ea5c255
Camilo Valdes <bolivar014@gmail.com>
2022-03-10 19:56:52 -0500

    Integrando rutas | ExpressJS - NodeJS

--- index.js
+++ index.js
@@ -7,10 +7,26 @@ const app = express();
 // Inicializamos puerto
 const port = 3000;
 
-// Callback
+// Callback - HOME
 app.get('/', (req, resp) => {
   // Retornamos respuesta.
   resp.send('Hola, mi servidor en express');
+});
+
+// Callback - Ruta Productos
+app.get('/new_route', (req, resp) => {
+  resp.send('Hola, esto es una nueva ruta');
+});
+
+// Callback - Ruta Productos
+app.get('/products', (req, resp) => {
+  // resp.send('Hola, ruta de productos');
+
+  // Retornamos respuesta - JSON
+  resp.json({
+    name: 'Product 1',
+    price: 1000
+  });
 });
 
 // Escucha de puerto
