d218a639dec01f9358999edd1fb1f8ce1d958270
Camilo Valdes <bolivar014@gmail.com>
2022-03-12 22:16:58 -0500

    control de cors

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Constructor routes | app
 const routerApi = require('./routes');
 
+// Importamos libreria cors
+const cors = require('cors');
+
 // Exporto middlewares de error
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
@@ -15,6 +18,26 @@ const port = 3000;
 
 // Middleware nativo EXPRES el cual permite retornar cadenas JSON
 app.use(express.json());
+
+
+// Habilitamos cualesquier dominio dentro del mismo origen
+const whitelist = ['http://localhost:8080', 'https://myapp.com'];
+
+// Configuramos options de cors validando que sean dominios validos
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)) {
+      // En caso que la URL este permitida, ejecuta callback
+      callback(null, true);
+    } else {
+      // Cuando es una URL no valida, ejecuta error
+      callback(new Error('No permitido'));
+    }
+  }
+}
+
+// Ejecutamos cors
+app.use(cors());
 
 // Callback - HOME
 app.get('/', (req, resp) => {
@@ -30,13 +53,13 @@ app.get('/new_route', (req, resp) => {
 // Asignamos control de rutas | llamamos función que se encarga de enrutar
 routerApi(app);
 
-//
+// Control - Logs de error
 app.use(logErrors);
 
-//
+// Control - BoomError
 app.use(boomErrorHandler);
 
-//
+// Control Error
 app.use(errorHandler);
 
 // Escucha de puerto
